0246765229b101c8f1332c690266b019f93710b6
Thierry Joux <tjoux@hotmail.com>
2019-09-19 10:00:43 -0700

    cleaned up code

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -9,14 +9,72 @@ class BinarySearchTree:
     self.left = None
     self.right = None
 
+
   def insert(self, value):
-    pass
+
+    if value == self.value:
+      return
+
+    if value < self.value:
+      if not self.left:
+        self.left = BinarySearchTree(value)
+      else:
+        self.left.insert(value)
+    
+    elif value > self.value:
+      if not self.right:
+        self.right = BinarySearchTree(value)
+      else:
+        self.right.insert(value)
+    
+
+
 
   def contains(self, target):
-    pass
+
+    if self.value == target:
+      return True
+
+    if self.left and target == self.left.value:
+      return True
+
+    if self.right and target == self.right.value:
+      return True
+
+    if target < self.value:
+
+      if self.left:
+        return self.left.contains(target)
+      else:
+        return False
+
+    else:
+      if self.right:
+        return self.right.contains(target)
+      else:
+        return False
+
 
   def get_max(self):
-    pass
+
+    num = self.value
+
+    #max can only be to the right
+    if self.right:
+      num = self.right.get_max()
+  
+    return num
+
 
   def for_each(self, cb):
-    pass
+    cb(self.value)
+
+    if self.right:
+      self.right.for_each(cb)
+
+    if self.left:
+      self.left.for_each(cb)
+
+
+  # def in_order_print():
+
